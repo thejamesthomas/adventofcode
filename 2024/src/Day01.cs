fab640bf6e474f10957411b95159c800ed0451da
James Thomas <thejamesthomas@gmail.com>
2024-12-01 15:39:38 -0600

    [2024] Implement Day 1 Part 1

--- 2024/src/Day01.cs
+++ 2024/src/Day01.cs
@@ -1,9 +1,35 @@
-﻿namespace src;
+using System;
+using System.Collections.Generic;
 
-public class Day01
+namespace src
 {
-    public static bool test()
+    public class Day01
     {
-        return true;
+        public static int distance(int x, int y)
+        {
+            return Math.Abs(x - y);
+        }
+
+        public static int distance(int[] x, int[] y)
+        {
+            return x.OrderBy(i => i).Zip(y.OrderBy(j => j), (a, b) => distance(a, b)).Sum();
+        }
+
+        public static int distance(string input)
+        {
+            var x = new List<int>();
+            var y = new List<int>();
+
+            var lines = input.Split("\n").Select(l => l.Trim());
+
+            System.Console.WriteLine("lines : " + lines);
+            foreach (var line in lines) {
+                string[] parts = line.Split("   ");
+                x.Add(Int32.Parse(parts[0].Trim()));
+                y.Add(Int32.Parse(parts[1].Trim()));
+            }
+
+            return distance(x.ToArray(), y.ToArray());
+        }
     }
 }
